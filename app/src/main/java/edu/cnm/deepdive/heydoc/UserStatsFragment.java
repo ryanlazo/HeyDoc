95c4adde19859076276f674fc64e2e953e868e22
JakeBat <jake_batchelor@hotmail.com>
2018-03-12 08:24:23 -0600

    WIP user_stats

--- app/src/main/java/edu/cnm/deepdive/heydoc/UserStatsFragment.java
+++ app/src/main/java/edu/cnm/deepdive/heydoc/UserStatsFragment.java
@@ -1,29 +1,22 @@
 package edu.cnm.deepdive.heydoc;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class UserStatsFragment extends Fragment {
 
-
   public UserStatsFragment() {
-    // Required empty public constructor
-  }
 
+  }
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_user_stats, container, false);
-  }
+    View view = inflater.inflate(R.layout.fragment_user_stats, container, false);
 
+    return view;
+  }
 }
