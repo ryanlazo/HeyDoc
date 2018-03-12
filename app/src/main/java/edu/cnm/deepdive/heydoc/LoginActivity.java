9a27c04e8dc995d7b91c81b0075f54899f40b11c
ryanlazo <ryanlazo12@gmail.com>
2018-03-12 09:04:01 -0600

    adding user stats page

--- app/src/main/java/edu/cnm/deepdive/heydoc/LoginActivity.java
+++ app/src/main/java/edu/cnm/deepdive/heydoc/LoginActivity.java
@@ -25,8 +25,7 @@ public class LoginActivity extends Activity {
     ed1 = (EditText) findViewById(R.id.editText);
     ed2 = (EditText) findViewById(R.id.editText2);
     b2 = (Button) findViewById(R.id.button2);
-    tx1 = (TextView) findViewById(R.id.textView3);
-    tx1.setVisibility(View.GONE);
+
 
     b1.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -39,10 +38,6 @@ public class LoginActivity extends Activity {
           startActivity(intent);
         } else {
           Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
-          tx1.setVisibility(View.VISIBLE);
-          tx1.setBackgroundColor(Color.RED);
-          counter--;
-          tx1.setText(Integer.toString(counter));
           if (counter == 0) {
             b1.setEnabled(false);
           }
